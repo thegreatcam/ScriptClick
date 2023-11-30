c25b20df07fab3eaf5810b187fb6c7acae8459ff
thegreatcam <98361191+thegreatcam@users.noreply.github.com>
2023-11-30 01:07:58 -0600

    Login and Sign Up
    The login and sign-up functionalities of Script Click

---
+++ login/ActivityRegisterBinding.java
@@ -0,0 +1,4 @@
+package com.example.scriptclick.ui.login;
+
+public class ActivityRegisterBinding {
+}
---
+++ login/LoginViewModelFactory.java
@@ -0,0 +1,26 @@
+package com.example.scriptclick.ui.login;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+
+import com.example.scriptclick.data.LoginDataSource;
+import com.example.scriptclick.data.LoginRepository;
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+public class LoginViewModelFactory implements ViewModelProvider.Factory {
+
+    @NonNull
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+            return (T) new LoginViewModel(LoginRepository.getInstance(new LoginDataSource()));
+        } else {
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
+}
